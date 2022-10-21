772f82a45691369b3c5d27d856d494c697e2003f
Tushti Sinha <you@example.com>
2022-10-21 12:16:48 +0530

    created the plots

---
+++ sportsAnalytics.R
@@ -0,0 +1,10 @@
+#clear all environment variables
+rm(list=ls())
+
+#let us load the library files
+library("dplyr")
+library("magrittr")
+library("ggplot2")
+
+# install.packages("dplyr")
+# install.packages("ggplot2")
